9f04702a1eadcc75ad2bd307a1561cccad07147b
Jacob Truong <103521402+jacobmtruong@users.noreply.github.com>
2022-10-04 15:50:45 -0700

    Add files via upload

---
+++ src/test/java/FestlookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestlookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
